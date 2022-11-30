b4bcd5ced97b19dace70287efff96bfd26ead0cb
Leon <82466943+LeonTheDev-io@users.noreply.github.com>
2022-11-30 21:11:41 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-          Made and deleted with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you | <a href="https://github.com/LeonTheDev-io/scpf-82-incompetence">Source Code</a> 
+          Made and deleted with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you | <a href="https://github.com/LeonTheDev-io/scpf-82-incompetence"> Source Code <img src="/github.svg" alt="Github Logo" className={styles.logo} /></a> 
       </footer>
     </>
   )
